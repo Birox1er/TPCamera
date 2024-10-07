3fd1198aa51913b2691c4dbdb6a092aaf51087ff
Alexis Le Bourhis <lebourhisalexis11@gmail.com>
2024-10-07 13:54:31 +0200

    Exo 5

--- CAMERAAA/Assets/TP_GreyBox/Script/Rail.cs
+++ CAMERAAA/Assets/TP_GreyBox/Script/Rail.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Rail : MonoBehaviour
 {
@@ -9,8 +10,7 @@ public class Rail : MonoBehaviour
 
     List<GameObject> children = new List<GameObject>();
 
-    private float length = 0;
-
+    [SerializeField] private float length = 0;
     private void Start()
     {
         for (int i = 0; i < transform.childCount; i++)
@@ -28,29 +28,81 @@ public class Rail : MonoBehaviour
         {
             length += Vector3.Distance(children[transform.childCount - 1].transform.position, children[0].transform.position);
         }
+        
     }
     private void OnDrawGizmos()
     {
-        Gizmos.color = new Color(0, 0.66f, 0.42f);
-        for(int i = 0;i < children.Count;i++)
+        if(children.Count > 0)
         {
-            if (i > 0)
+            Gizmos.color = new Color(0, 0.66f, 0.42f);
+            for (int i = 0; i < children.Count; i++)
             {
-                Gizmos.DrawLine(children[i - 1].transform.position, children[i].transform.position);
+                if (i > 0)
+                {
+                    Gizmos.DrawLine(children[i - 1].transform.position, children[i].transform.position);
+                }
+            }
+            if (isLoop)
+            {
+                Gizmos.DrawLine(children[children.Count - 1].transform.position, children[0].transform.position);
             }
         }
-        if(isLoop)
-        {
-            Gizmos.DrawLine(children[children.Count - 1].transform.position, children[0].transform.position);
-        }
+        
     }
 
     public float GetLength() { return length;}
 
     public Vector3 GetPosition(float distance)
     {
-          
+        float distanceWithinBornes = 0;
         
+        Vector3 position = children[0].transform.position;
+        if (isLoop)
+        {
+            distanceWithinBornes = distance % length;
+        }
+        else
+        {
+            distanceWithinBornes = Mathf.Clamp(distance, 0, length);
+        }
+        float currentDist = 0;
+        Vector3 nextPoint = Vector3.zero;
+        float percent = 0;
+        for (int i = 0; i < children.Count; i++)
+        {
+            
+            if (i == children.Count - 1)
+            {
+                if (isLoop)
+                {
+                    nextPoint = children[0].transform.position;
+                }
+                else
+                {
+                    return children[children.Count - 1].transform.position;
+                }
+            }
+            else
+            {
+                nextPoint = children[i + 1].transform.position;
+            }
+
+            if (distanceWithinBornes >= currentDist && distanceWithinBornes < currentDist + Vector3.Distance(children[i].transform.position, nextPoint))
+            {
+                percent = (distanceWithinBornes - currentDist) / Vector3.Distance(children[i].transform.position, nextPoint);
+                
+                position = Vector3.Lerp(children[i].transform.position, nextPoint, percent);
+                break;
+            }
+            else
+            {
+
+                currentDist += Vector3.Distance(children[i].transform.position, nextPoint);
+            }
+
+        }
+        
+        return position;
     }
 
 }
